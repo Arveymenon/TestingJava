c177cccda9bae2ec5c213e9a57d5ecd208639a3b
arveymenon <arveymenon@gmail.com>
2022-03-07 07:42:32 +0530

    Add config added

---
+++ SprintBootMavern/src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,39 @@
+package com.example.demo.student;
+
+import net.bytebuddy.utility.RandomString;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.nio.charset.Charset;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+            return args -> {
+
+                List<Student> students = new ArrayList<>();
+                for(int i = 0; i < 10; i++){
+                    int date = 1 + (int) (Math.random() * 10);
+                    int month = 1 + (int) (Math.random() * 10);
+                    int year = 2000 + (int)((Math.random() * 10));
+
+                    students.add(
+                        new Student(
+                                "Name",
+                                "email",
+                                LocalDate.of(year, Month.of(month), date),
+                                25
+                        )
+                    );
+                }
+                repository.saveAll(students);
+            };
+    }
+}
