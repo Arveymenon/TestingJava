3c25cf2995551bc29fe5e937e57d65cad17405fc
arveymenon <arveymenon@gmail.com>
2022-03-19 10:22:38 +0530

    update

---
+++ Concepts/IOStream/ByteStream.java
@@ -0,0 +1,28 @@
+package Concepts.IOStream;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.swing.plaf.synth.SynthStyleFactory;
+
+public class ByteStream {
+    FileInputStream inputStream = null;
+    FileOutputStream outputStream = null;
+    public ByteStream(){
+
+        try {
+            this.inputStream = new FileInputStream("/Users/arulvinayak/Desktop/Projects/java/Concepts/assets/source.txt");
+            this.outputStream = new FileOutputStream("/Users/arulvinayak/Desktop/Projects/java/Concepts/assets/dest.txt");
+    
+            int content;
+            while((content = inputStream.read()) != -1){
+                // this.outputStream.write((byte) content);
+                this.outputStream.write((char) content);
+            }
+
+        } catch(IOException e){
+            System.err.println(e);
+        }
+    }
+}
--- entrypoint.java
+++ entrypoint.java
@@ -1,20 +1,8 @@
-import Concepts.Abstraction.*;
-// import Concepts.Interfacing.*;
-import Concepts.Abstraction.Enums.Units;
+import Concepts.IOStream.ByteStream;
 
 public class entrypoint{
 
     public static void main(String[] args){
-
-        System.out.println("Create Gaming PC");
-        System.out.println(" ");
-
-        CorsairX1 storage = new CorsairX1();
-        storage.setName("Corsair Test").setSize(20, Units.GB, "SSD");
-        
-        storage.printDetails();
-        // GamingPC pc = new GamingPC();
-        // pc.getPC();
+        new ByteStream();
     }
-
 }
